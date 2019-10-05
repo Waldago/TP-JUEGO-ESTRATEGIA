680bc7832cc71ba19d0aed39cae090eeec72ee4e
Waldago <54184873+Waldago@users.noreply.github.com>
2019-10-04 21:00:03 -0300

    Update Caballero.java

--- Caballero.java
+++ Caballero.java
@@ -5,8 +5,9 @@ public class Caballero extends Unidad{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Unidad c1 = new Caballero(2,3);
-		Unidad c2 = new Caballero(2,1);
+		Caballero c1 = new Caballero(2,3);
+		Caballero c2 = new Caballero(2,1);
+		Caballero c3 = new Caballero(2,2);
 		c1.atack(c2);
 		System.out.println(c2);
 		c1.atack(c2);
@@ -18,6 +19,13 @@ public class Caballero extends Unidad{
 		System.out.println(c1);	
 		c2.move("cnnnf");
 		c2.atack(c1);
+		c1.atack(c3);
+		System.out.println(c1);
+		c1.pocionA();
+		c1.atack(c3);
+		System.out.println(c1);
+		System.out.println(c2);
+		c2.atack(c3);
 	}
 
 	private boolean horse;
@@ -32,28 +40,33 @@ public class Caballero extends Unidad{
 	}
 
 	private void revelde() {
-		if(this.getCont()<3) {
-			this.setHorse(false);
+		if(this.getCont()==3) {
+			this.setHorse(true);
 		}
-		else this.setHorse(true);
+		return;
 	}
 
 	public void atack(Unidad b) {
 		if(this.dead()==true) {
-			System.out.println("La unidad esta muerta");
+			System.out.println("The Unit is dead!");
 			return;
 		}
 		this.revelde();
 		if (this.distance(b)<=3 && this.distance(b)>=1) {
 			if (this.isHorse()!=true) {
 				b.setHp(b.getHp()-this.getP_atk());
-				cont++;
+				this.setCont(cont+1);
 			}
 			else System.out.println("El caballo esta revelde");
 		}
 		else System.out.println("It out of Range");
 	}
 
+	public void pocionA() {
+		this.setHorse(false);
+		this.setCont(0);
+	}
+	
 	public boolean isHorse() {
 		return horse;
 	}
